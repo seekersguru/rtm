fd994153c948d69ef276ba101c68a6248851e453
Manishkokse <kokse.manish@gmail.com>
2016-04-18 13:16:25 +0530

    Update views.py

--- places/views.py
+++ places/views.py
@@ -220,7 +220,7 @@ def iternary_detail(request,iternary_id):
         mobile=phone,comment=comment,date=datetime.now())
         obj.save()
         sub="Mail From Royal Trip Maker"
-	msg='From : %s  \n Message : %s'%((email+"   "+"Name :"+name+" "+"Mobile:"+phone),comment)
+	msg='From : %s  \n Enquiry For: %s \n Message : %s'%((email+"   "+"Name :"+name+" "+"Mobile:"+phone),iternaryenquiry,comment)
 	frm=email
 	to_us=[settings.EMAIL_HOST_USER]
 	send_mail(sub,msg,frm,to_us,fail_silently=False)
